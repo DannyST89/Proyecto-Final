5786d3de8fae2463056957485848ce4070ab5d12
DannyST89 <dsdevelopercr@outlook.com>
2022-04-15 14:27:25 -0600

    Se crea la base de datos y se hace el renderizado

--- Etapa 01/DATA.sql
+++ Etapa 01/DATA.sql
@@ -2,9 +2,33 @@
 /*En este script unicamente agregamos datos de prueba,
   ya que necesitaba verificar el funcionamiento correcto
   de cada funcionalidad del programa.*/
-  USE SUPERMERCADODB
-  GO
+ USE SUPERMERCADODB
+ GO
   /*Agregamos datos a la tabla EMPLEADOS*/
-  INSERT INTO EMPLEADOS(NOMBRE,PRIMER_APELLIDO,SEGUNDO_APELLIDO,TELEFONO,CORREO,
-						DIRECCION,CARGO,FECHA_INGRESO,NOMBRE_USUARIO,CONTRASENIA)
-						VALUES('Carlos','Arguedas','Mora','84751210','ArgM@gmail.com','Atenas','Cajero',GETDATE(),'CMora','Carlos1299')
+ INSERT INTO EMPLEADOS(NOMBRE,PRIMER_APELLIDO,SEGUNDO_APELLIDO,TELEFONO,CORREO,DIRECCION,CARGO,FECHA_INGRESO,NOMBRE_USUARIO,CONTRASENIA)
+
+		VALUES('Carlos','Arguedas','Mora','84751210','ArgM@gmail.com','Atenas','Cajero',GETDATE(),'CMora','Carlos1299')
+
+GO
+
+INSERT INTO PROVEEDORES(NOMBRE_PROVEEDOR,DIRECCION,TELEFONO,EXTENSION,CORREO,NUMERO_CUENTA)
+						VALUES('Coca Cola','Alajuela','24561212','101','ventaCR@CocaCola.com','IBAN124570154721'),
+							  ('Dos Pinos','Alajuela','24601589','115','mercadeoDospinos@DosPinos.com','00215024574034'),
+							  ('Cafe 1820','Cartago','23568961','103','ventas@cafe1820.com','IBAN124056534')
+							 
+GO
+
+INSERT INTO PRODUCTOS(ID_PRODUCTO,ID_EMPLEADO,ID_PROVEEDOR,DESCRIPCION,CANTIDAD,PRECIO_UNIDAD)
+		VALUES(7896359014811,1,1,'Fanta Fresa 300 ml',3,450.00),
+			  (0215487021548,1,2,'Leche Semidescremada 1L',1,1050.00),
+			  (4125031006598,1,3,'Cafe 1820 Molido Puro Tueste Oscuro 500G',1,3520.00)
+
+
+
+
+
+SELECT * FROM EMPLEADOS
+SELECT * FROM PROVEEDORES
+SELECT * FROM PRODUCTOS
+SELECT * FROM INVENTARIOS
+
--- Etapa 01/PROCEDIMIENTOS Y TRIGGERS.sql
+++ Etapa 01/PROCEDIMIENTOS Y TRIGGERS.sql
@@ -4,15 +4,29 @@
 
 /*En este script crearémos todos los procedimientos almacenados, triggers necesarios para
   solucionar el problema */
-  USE SUPERMERCADODB
-  GO
-  /*Creámos un trigger que al ingresar un nuevo EMPLEADO se agregue el estado de activo*/
-  CREATE TRIGGER TR_CAMBIAR_ESTADO_EMPLEADO
-  ON EMPLEADOS
-  FOR INSERT
-  AS
-	DECLARE	@ESTADO VARCHAR(3)
-	SELECT	@ESTADO = ESTADO FROM inserted
-	INSERT INTO EMPLEADOS(ESTADO)
-			VALUES('ACT')
-	
+USE SUPERMERCADODB
+GO
+
+/*Desarrollamos un trigger que se dispare al insertar un producto en la tabla de PRODUCTOS,
+  se debe de insertar ese producto en la tabla de inventarios, en cado de ya exister ese id,
+  unicamente se suma a la cantidad de existencia la cantidad que fue ingresada.*/
+CREATE OR ALTER TRIGGER TR_FI_PRODUCTO
+ON PRODUCTOS
+FOR INSERT
+AS
+	DECLARE @ID_PRODUCTO VARCHAR(50), @DESCRIPCION VARCHAR(50), @CANTIDAD INT,@PRECIO_UNIDAD DECIMAL(10,2)
+	SELECT @ID_PRODUCTO = ID_PRODUCTO FROM inserted
+	SELECT @DESCRIPCION = DESCRIPCION FROM inserted
+	SELECT @CANTIDAD = CANTIDAD FROM inserted
+	SELECT @PRECIO_UNIDAD = PRECIO_UNIDAD FROM inserted
+	IF(EXISTS(SELECT ID_PRODUCTO FROM INVENTARIOS WHERE ID_PRODUCTO = @ID_PRODUCTO))
+		UPDATE INVENTARIOS SET EXISTENCIA += @CANTIDAD
+	ELSE
+		INSERT INTO INVENTARIOS (ID_PRODUCTO,DESCRIPCION,EXISTENCIA,PRECIO_UNIDAD)
+			VALUES(@ID_PRODUCTO,@DESCRIPCION,@CANTIDAD,@PRECIO_UNIDAD)
+GO
+
+
+
+
+
